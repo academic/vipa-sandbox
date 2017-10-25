dea59476cab1a456f494495791b2049661dcde64
Behram ÇELEN <behramcelen@gmail.com>
2017-10-25 03:26:22 +0200

    add zipping function to zip sandbox

--- etc/bin/update_sandbox.sh
+++ etc/bin/update_sandbox.sh
@@ -6,3 +6,7 @@ composer install --no-interaction --prefer-dist &&
 bower install --force --silent &&
 php app/console assetic:dump --env=prod &&
 chmod -R 777 app/cache app/logs
+cd .. &&
+rm -rf sandbox.zip &&
+zip -r sandbox.zip vipa/ &&
+rm -rf vipa/
